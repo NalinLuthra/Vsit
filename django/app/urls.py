e2f94d58dccea14615102f81de52fb08fb7663fc
Aditya Arora <adi.arora.797@gmail.com>
2019-02-17 05:32:24 +0530

    Update urls.py

--- django/app/urls.py
+++ django/app/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('camera/', views.camera, name='camera')
+    path('camera/cholesterol/', views.cholesterol_, name='cholesterol_'),
+    path('camera/bilirubin/', views.bilirubin_, name='bilirubin_'),
+    path('camera/catarct/', views.catarct_, name='catarct_'),
 ]
