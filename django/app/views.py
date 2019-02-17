11c8f52e7bd88f39e49d894c5f1df98e344f558c
adi-797 <adi.arora.797@gmail.com>
2019-02-17 22:02:11 +0530

    updated views.py

--- django/app/views.py
+++ django/app/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from django.http import HttpResponse
 from django.shortcuts import render
 import numpy as np, cv2, os, imutils
+from statistics import mode
 #from . import NameForm
 
 class SignUp(generic.CreateView):
@@ -68,7 +69,6 @@ def nothing(temp):
 def camera():
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     face_cascade = cv2.CascadeClassifier(os.path.join(BASE_DIR, 'haarcascade_frontalface_alt.xml'))
-    print ("hello")
 
     try:
         camera=cv2.VideoCapture(1)
@@ -95,10 +95,10 @@ def camera():
             coordinates_eye.append((x,y,w,h))
     ##        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),1)
             #cv2.line(frame,(int(x+w/2),int(y)),(int(x+w/2),int(y+h/2)),(255,0,0),1)
-            cv2.line(frame,(int(x+w/4.2),int(y+h/2.2)),(int(x+w/2.5),int(y+h/2.2)),(0,255,0),1)
-            cv2.line(frame,(int(x+w/4.2),int(y+h/3)),(int(x+w/2.5),int(y+h/3)),(0,255,0),1)
-            cv2.line(frame,(int(x+w/4.2),int(y+h/3)),(int(x+w/4.2),int(y+h/2.2)),(0,255,0),1)
-            cv2.line(frame,(int(x+w/2.5),int(y+h/3)),(int(x+w/2.5),int(y+h/2.2)),(0,255,0),1)
+            #cv2.line(frame,(int(x+w/4.2),int(y+h/2.2)),(int(x+w/2.5),int(y+h/2.2)),(0,255,0),1)
+            #cv2.line(frame,(int(x+w/4.2),int(y+h/3)),(int(x+w/2.5),int(y+h/3)),(0,255,0),1)
+            cv2.line(frame,(int(x+w/4.2),int(y+h/3)),(int(x+w/4.2),int(y+h/2.2)),(0,255,0),1) #left
+            cv2.line(frame,(int(x+w/2.5),int(y+h/3)),(int(x+w/2.5),int(y+h/2.2)),(0,255,0),1) #right
             
             #------------ estimation of distance of the human from camera--------------#
             d=10920.0/float(w)
@@ -132,7 +132,8 @@ def camera():
                     cx = int(M['m10']/M['m00'])
                     cy = int(M['m01']/M['m00'])
                     pupil.append((cx,cy))
-                    cv2.line(roi,(cx,cy),(cx,cy),(255,0,255),3)
+                    cv2.line(roi,(cx,cy),(cx,cy),(255,0,255),10)
+                    
                 #print cx,cy
             #-------- checking for one countor presence --------------------#
             elif len(contours)==1:
@@ -146,123 +147,74 @@ def camera():
                     cy = int(M['m01']/M['m00'])
                     pupil.append((cx,cy))
                     #print cx,cy
-                    cv2.line(roi,(cx,cy),(cx,cy),(0,0,255),3)
+                    cv2.line(roi,(cx,cy),(cx,cy),(0,0,255),20)
+        
             else:
                 denominator+=1
-                #print "iris not detected"
+                #print "iris not detected"            
     
         cv2.putText(frame,'Press "ESC" to capture.',(20,30), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,0),2,cv2.LINE_AA)
         cv2.imshow("frame",frame)
-        sample2 = sample[x:x+w, y:y+h]
-        cv2.imshow("frame2", sample2)
+        
         #cv2.imshow("eye",image)
         if cv2.waitKey(30)==27 & 0xff:
-            sample = sample2
+            ret, sample = camera.read()
             break
     camera.release()
     #print ("accurracy=",(float(numerator)/float(numerator+denominator))*100)
     cv2.destroyAllWindows()
 
-    x = 0
-    y = 0
+    x2 = 0
+    y2 = 0
+
+    a = mode([x for x in range(len(pupil))])
+
+    for i in range(len(pupil)):
+        if pupil[i][0] - a[0] > 20 or pupil[i][1] - a[1]>20:
+             pupil.remove(i+1)
+
 
     for index in range(len(pupil)):
 
-            x+= pupil[index][0]
-            y+= pupil[index][1]
+            x2+= pupil[index][0]
+            y2+= pupil[index][1]
 
 
     cx, cy = 0,0
-    cx = x/len(pupil)
-    cy = y/len(pupil)
+    cx = x2/len(pupil)
+    cy = y2/len(pupil)
 
-    return sample,cx,cy
+    return sample,x2,y2
 
 def cholesterol(sample, cx,cy):
+    sample = imutils.resize(sample, width=640)
     blur = cv2.medianBlur(sample,5)
     bil = cv2.bilateralFilter(blur,5,1000,1000)
-    copy_orig = bil.copy()
+
+    y = cy-23
+    h = 50
+    x = cx - 60
+    w = 110
+
     gray = cv2.cvtColor(bil,cv2.COLOR_BGR2GRAY)
-    ret,thresh1 = cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV)
-
-    cv2.imshow('Masked Image', thresh1)
-
-    # Trackbar for changing the threshold value.
-    cv2.createTrackbar('min_value','Masked Image',0,255,nothing)
-
-    # Trackbar execution.
-    while(1):
-        cv2.imshow('Masked Image', thresh1)
-
-        min_value = cv2.getTrackbarPos('min_value', 'Masked Image')
-        ret,thresh1 = cv2.threshold(gray,min_value,255,cv2.THRESH_BINARY_INV)
-
-        # If ESC is pressed, execution breaks from the loop.
-        k = cv2.waitKey(37)
-        if k == 27:
-            cv2.destroyAllWindows()
-            break
-
-    # Contour detection.
-    contours, hierarchy = cv2.findContours(thresh1, 1, 2)
-    cont = max(contours, key=cv2.contourArea)
 
     # Center and radius detection of pupil.
-    (x,y), pupil_radius = cv2.minEnclosingCircle(cont)
-    pupil_center = (int(x), int(y))
-    pupil_radius = int(pupil_radius)
+    pupil_center = (cx, cy)
+    pupil_radius = 5
     cv2.circle(sample, pupil_center, pupil_radius, (0,0,255), 2)
 
     # Multiplier based on general ratio of radius of iris to pupil.
     iris_radius = pupil_radius*4
-
-    # Radius detection for Iris.
-    (cx,cy) = (int(x)+iris_radius, int(y))
-
-    if cx > 640:
-        cx = 640
-
-    if cy > 640:
-        cy = 640
-
-    try:
-        while(1):
-
-            if cx > 639:
-                color = gray[cy,cx]
-                break
-            color = gray[cy,cx]
-            if color>110:
-                cx = cx-1
-            else:
-                break
-
-        iris_radius = cx-pupil_center[0]
-
-    except:
-        iris_radius = pupil_radius*4
-
     
     cv2.circle(sample,pupil_center,iris_radius , (0,0,255), 2)
 
     # Finding the minimum bounded rectangle.
     cv2.circle(sample,pupil_center,iris_radius , (255,255,255), -1)
-    gray1 = cv2.cvtColor(sample,cv2.COLOR_BGR2GRAY)
-    ret,thresh2 = cv2.threshold(gray1,250,255,cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(thresh2, 1, 2)
-
-    if len(contours)>0:
-        maxC = max(contours, key=cv2.contourArea)
-        x,y,w,h = cv2.boundingRect(maxC)
-        cv2.rectangle(sample,(x,y),(x+w,y+h),(255,255,255),2)
 
     # Cropping to a standard size.
-    cropped = copy_orig[y:y+h, x:x+w]
-    cropped = cv2.resize(cropped, (240,240))
 
     mask = np.zeros(sample.shape, dtype = "uint8")
     cv2.circle(mask,pupil_center,iris_radius , (255,255,255), -1)
-    cv2.circle(copy_orig, pupil_center, int(pupil_radius*2.2), (0,0,0), -1)
     mask = mask[y:y+h, x:x+w]
     mask = cv2.resize(mask, (240,240))
     mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
@@ -270,6 +222,10 @@ def cholesterol(sample, cx,cy):
     copy_orig = cv2.resize(copy_orig, (240,240))
     res = cv2.bitwise_and(copy_orig,copy_orig, mask = mask)
 
+    cv2.imshow("a", res)
+    cv2.imshow("abb", sample)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     ##res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
     ##imga = np.asarray(res,dtype=np.float64)
     ##pol = topolar(imga)
@@ -288,7 +244,8 @@ def cholesterol(sample, cx,cy):
 
     ratio_of_grey = mat/total
 
-    return ratio_of_grey
+    #return ratio_of_grey
+    return 20
 
 def bilirubin(sample, cx,cy):
     image = imutils.resize(sample, width=640, height=480)
@@ -322,7 +279,41 @@ def bilirubin(sample, cx,cy):
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-    return average
+    return 20
+
+def cataract(sample, cx,cy):
+    image = imutils.resize(sample, width=640, height=480)
+    image = cv2.medianBlur(image,5)
+    image = cv2.bilateralFilter(image,5,1000,1000)
+   # image = image[c] TODO
+    frame = image.copy()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([20,50,120]) #all shades of yellow
+    upper_red = np.array([60,255,255])
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame,frame, mask = mask)
+    image = res.copy()
+    res = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
+    y, x = res.shape[:2]
+    total = 0
+    mat = 0
+    for i in range(x):
+        for j in range(y):
+            arr = res[j, i]
+            if arr == 0:
+                continue
+            mat+=1
+            total+=arr
+
+    average = float(total)/float(mat)
+    average = 1- np.interp(average, [0,255], [0,1])
+    cv2.imshow("gray", res)
+    cv2.imshow("masked", image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    return 20
 
 def bilirubin_(request):
     sample_frame,cx,cy = camera()
@@ -332,6 +323,10 @@ def bilirubin_(request):
     return HttpResponse('bilirubin')
 
 def catarct_(request):
+    sample_frame,cx,cy = camera()
+
+    cataract_level = catarct(sample_frame,cx,cy)
+
     return HttpResponse('catarct')
 
 def cholesterol_(request):
@@ -339,5 +334,5 @@ def cholesterol_(request):
 
     cholesterol_level = cholesterol(sample_frame,cx,cy)
 
-    return HttpResponse("Done")
+    return HttpResponse("cholesterol")
 
