e6cc3f3378286d5caf2c62a47fb0dcc6878a143d
nalin1997 <nalin.luthra@gmail.com>
2019-02-17 15:02:05 +0530

    Whatsapp

---
+++ whatsapp.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client 
+ 
+account_sid = 'ACd0767ea59a8b91740f8c3cc02e18e627' 
+auth_token = 'edc7fa535e6bf70fdb5e5ccb6aea9e59' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create( 
+                              from_='whatsapp:+14155238886',  
+                              body='Please have your medicine',      
+                              to='whatsapp:+919999795087' 
+                          ) 
+ 
+print(message.sid)
